05798135aa6f999d13c6cac4c9bd0dcee9207b0d
stephenwiafe <swiafe.dev@gmail.com>
2025-01-19 18:35:04 -0500

    Add functions boilerplate

---
+++ tree.js
@@ -0,0 +1,47 @@
+import { Node } from "./node";
+
+class Tree {
+    #root = null;
+
+    constructor(array){
+        this.arr = arr
+    }
+
+    buildTree(array){
+    }
+
+    insert(value){
+    }
+
+    deleteItem(value){
+    }
+
+    find(value){
+    }
+
+    levelOrder(callback){
+    }
+
+    inOrder(callback){
+    }
+
+    preOrder(callback){
+    }
+
+    postOrder(callback){
+    }
+
+    height(node){
+    }
+
+    depth(node){
+    }
+
+    isBalanced(){
+    }
+
+    rebalance(){
+    }
+}
+
+export {Tree};
