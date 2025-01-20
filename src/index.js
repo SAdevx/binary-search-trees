7f7d6f4e8863de759a7e6895e7963ce2f141eb59
stephenwiafe <swiafe.dev@gmail.com>
2025-01-20 12:23:35 -0500

    Implement buildTree function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import { Tree } from "./tree";
+import { Tree } from "./tree.js";
+
+let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+test.buildTree(test.array);
--- src/tree.js
+++ src/tree.js
@@ -1,13 +1,46 @@
-import { Node } from "./node";
+import { Node } from "./node.js";
 
 class Tree {
     #root = null;
 
     constructor(array){
-        this.arr = arr
+        this.array = this.getUniqueElements(array);
+         console.log(this.array)
     }
 
     buildTree(array){
+        let n = array.length;
+        if(n === 0) return null;
+
+        let mid = Math.floor((n-1)/2);
+        let root = new Node(array[mid]);
+
+        let q = [{node: root, range: [0,n-1]}];
+        let frontIndex = 0;
+      
+        while(frontIndex < q.length){
+            let front = q[frontIndex];
+            let curr = front.node;
+            let [s,e] = front.range;
+            let index = s + Math.floor((e-s)/2);
+            
+            if(s < index){
+                let midLeft = s + Math.floor((index-1-s)/2);
+                let left = new Node(array[midLeft]);
+                curr.left = left;
+                q.push({node: left, range: [s, index - 1]});
+            }
+
+            if(e > index){
+                let midRight = index + 1 + Math.floor((e-index-1)/2);
+                let right = new Node(array[midRight]);
+                curr.right = right;
+                q.push({node: right, range: [index+1,e]});
+            }
+            frontIndex++;
+        }
+        this.#root = root;
+        return root;
     }
 
     insert(value){
@@ -41,6 +74,10 @@ class Tree {
     }
 
     rebalance(){
+    }
+
+    getUniqueElements(arr){
+        return Array.from(new Set(arr)).sort((a,b) => a - b);
     }
 }
 
